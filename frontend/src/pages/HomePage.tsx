830a9745771254692d64c7edc6d9baa6cbe93ee1
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-12-08 22:32:20 +0100

    feat: Updating content in homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,23 @@
+import BookCarousel from "@/components/CarouselDisplay";
 import Hero from "@/components/Hero";
+import SearchBar from "@/components/SearchBar";
+import booksData from "@/books.json";
+import { useState } from "react";
 
 export default function HomePage() {
-  return <Hero />;
+  const [search, setSearch] = useState("");
+
+  const randomBooks = booksData.random;
+  const horrorBooks = booksData.horror;
+  const featuredBooks = booksData.featured;
+
+  return (
+    <div className="flex-col w-full">
+      <Hero />
+      <SearchBar onSearch={setSearch} />
+      <BookCarousel title={"A la une"} books={featuredBooks} />
+      <BookCarousel title={"Selection Aléatoire"} books={randomBooks} />
+      <BookCarousel title={"Horreur"} books={horrorBooks} />
+    </div>
+  );
 }
