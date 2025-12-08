1fc5f20dec3cfcca4b52a1aa705aea571fe1a69f
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-12-08 22:31:50 +0100

    feat: Updating RootLayout css

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="container mx-auto flex min-h-screen flex-1 justify-center py-10">
+      <main className="sm:px-20 container mx-auto flex min-h-screen flex-1 justify-center py-10">
         <Outlet />
       </main>
       <Footer />
