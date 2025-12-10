a888fab417c039d69b01a11d4ac94984b224adca
GuillemotClement <guillemot.clement@protonmail.com>
2025-12-10 11:41:55 +0100

    temp

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,9 +1,15 @@
-import { Outlet } from "@tanstack/react-router";
+import { Link, Outlet } from "@tanstack/react-router";
 
 export default function RootLayout() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       {/* <Header /> */}
+      {/* TEMP========================= */}
+      <header className="flex gap-x-5">
+        <Link to="/register" className="underline">Register</Link>
+        <Link to="/login" className="underline">Login</Link>
+      </header>
+      {/*  ============================== */}
       <main className="container mx-auto flex min-h-screen flex-1 justify-center py-10">
         <Outlet />
       </main>
