a91646843f41f4f26051814f25274aa571d3ef89
Mohinisch <schirinzi.mohini@gmail.com>
2026-01-13 11:15:00 +0100

    feat(db): add injectable DrizzleModule and update AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { DrizzleModule } from './db/drizzle.module';
 
 @Module({
-  imports: [AuthModule, BooksModule, UserModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [
+    DrizzleModule.forRoot({
+    connectionString: process.env.DATABASE_URL,
+  }),
+    AuthModule, 
+    BooksModule, 
+    UserModule, 
+    CategoryModule
+  ],
+
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
