2ed72e139f2248a5886cf0d2e424ec07967a6b91
Mohinisch <schirinzi.mohini@gmail.com>
2026-01-13 15:43:05 +0100

    docs(category): add Swagger decorators to CreateCategoryDto

--- backend/src/category/dto/create-category.dto.ts
+++ backend/src/category/dto/create-category.dto.ts
@@ -1,11 +1,21 @@
 import { IsString, IsNotEmpty, IsBoolean, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 
 export class CreateCategoryDto {
+  @ApiProperty({
+  description: 'Nom de la catégorie',
+  example: 'Science Fiction',
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(100)
   name: string;
-  
+
+  @ApiProperty({
+    description: 'Statut de la catégorie',
+    example: true,
+  })
   @IsBoolean()
   @IsNotEmpty()
   isActive?: boolean = true;
