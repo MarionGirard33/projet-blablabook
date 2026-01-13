243325dfa2153534d1ffd2c4e5df9f8d8362ac28
Mohinisch <schirinzi.mohini@gmail.com>
2026-01-13 11:31:57 +0100

    feat(category): add CreateCategoryDto and remove UpdateCategoryDto

--- backend/src/category/dto/create-category.dto.ts
+++ backend/src/category/dto/create-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCategoryDto {}
+import { IsString, IsNotEmpty, IsBoolean, MaxLength } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+  
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive?: boolean = true;
+}
