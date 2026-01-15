50579044731f621acde02402984b50cdc0c6f0b7
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-01-15 11:03:42 +0100

    feat: Update query key for each useQuery

--- frontend/src/hooks/useExternalBooks.ts
+++ frontend/src/hooks/useExternalBooks.ts
@@ -9,26 +9,26 @@ export const useExternalBooks = ({ mode, param }: UseExternalBooksOptions) => {
   switch (mode) {
     case "search":
       return useQuery<ExternalBook[]>({
-        queryKey: ["externalBooks", param],
+        queryKey: ["externalBooks", mode, param],
         queryFn: () =>
           searchExternalBooks({ type: "searchText", searchText: param! }),
-        staleTime: 1000 * 60 * 5,
+        staleTime: 1000 * 60 * 5, //5mins
         enabled: !!param && param.length > 0,
       });
 
     case "random":
       return useQuery<ExternalBook[]>({
-        queryKey: ["random-external-books"],
+        queryKey: ["random-external-books", mode],
         queryFn: () => searchExternalBooks({ type: "random" }),
-        staleTime: 1000 * 60 * 5,
+        staleTime: 1000 * 60 * 5, //5mins
       });
 
     case "category":
       return useQuery<ExternalBook[]>({
-        queryKey: ["by-category-external-books", param],
+        queryKey: ["by-category-external-books", mode, param],
         queryFn: () =>
           searchExternalBooks({ type: "category", categoryName: param! }),
-        staleTime: 1000 * 60 * 5,
+        staleTime: 1000 * 60 * 5, //5mins
       });
   }
 };
