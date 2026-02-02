42a1cd9e783e75590db0b5b5ffa8a0f9e859eb55
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-02-02 10:37:19 +0100

    fix: Update hook

--- frontend/src/hooks/useExternalBooks.ts
+++ frontend/src/hooks/useExternalBooks.ts
@@ -6,29 +6,31 @@ import type {
 import { searchExternalBooks } from "@/api/externalBooks";
 
 export const useExternalBooks = ({ mode, param }: UseExternalBooksOptions) => {
-  switch (mode) {
-    case "search":
-      return useQuery<ExternalBook[]>({
-        queryKey: ["externalBooks", mode, param],
-        queryFn: () =>
-          searchExternalBooks({ type: "searchText", searchText: param! }),
-        staleTime: 1000 * 60 * 5, //5mins
-        enabled: !!param && param.length > 0,
-      });
+  let queryKey: any[] = [];
+  let queryFn: () => Promise<ExternalBook[]>;
+  let enabled = true;
 
-    case "random":
-      return useQuery<ExternalBook[]>({
-        queryKey: ["random-external-books", mode],
-        queryFn: () => searchExternalBooks({ type: "random" }),
-        staleTime: 1000 * 60 * 5, //5mins
-      });
-
-    case "category":
-      return useQuery<ExternalBook[]>({
-        queryKey: ["by-category-external-books", mode, param],
-        queryFn: () =>
-          searchExternalBooks({ type: "category", categoryName: param! }),
-        staleTime: 1000 * 60 * 5, //5mins
-      });
+  if (mode === "search") {
+    queryKey = ["externalBooks", mode, param];
+    queryFn = () =>
+      searchExternalBooks({ type: "searchText", searchText: param! });
+    enabled = !!param && param.length > 0;
+  } else if (mode === "random") {
+    queryKey = ["random-external-books", mode];
+    queryFn = () => searchExternalBooks({ type: "random" });
+  } else if (mode === "category") {
+    queryKey = ["by-category-external-books", mode, param];
+    queryFn = () =>
+      searchExternalBooks({ type: "category", categoryName: param! });
+  } else {
+    enabled = false;
+    queryFn = async () => [];
   }
+
+  return useQuery<ExternalBook[]>({
+    queryKey,
+    queryFn,
+    staleTime: 1000 * 60 * 5,
+    enabled,
+  });
 };
