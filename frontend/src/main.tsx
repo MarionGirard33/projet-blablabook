42fc40184be7a05670736116cb7d7c111b5e582d
Mohinisch <schirinzi.mohini@gmail.com>
2026-02-03 19:09:10 +0100

    feat: add toaster sonner to switch alert

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./routes/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/sonner";
 import "./index.css";
 
 declare module "@tanstack/react-router" {
@@ -24,8 +25,9 @@ globalThis.__TANSTACK_QUERY_CLIENT__ = queryClient;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+          <Toaster position="bottom-right" richColors />
+          <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
-      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
